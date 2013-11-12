cb807f9a4be0b90a3a6edddb9ed3fa3eeb2f0d7c
Noel Dawe <noel.dawe@gmail.com>
2013-11-12 01:22:06 -0800

    bump version to 3.0 (YR3)

--- yellowhiggs/info.py
+++ yellowhiggs/info.py
@@ -10,7 +10,7 @@ Author: Noel Dawe
 
 """
 
-VERSION = '1.0a4'
+VERSION = '3.0'
 URL = 'https://end.web.cern.ch/end/projects/yellowhiggs/'
 DOWNLOAD_URL = 'http://pypi.python.org/packages/source/y/yellowhiggs/yellowhiggs-%s.tar.gz' % VERSION
 
