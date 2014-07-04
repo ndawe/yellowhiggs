cb659184c307f66c5b3958edb2bff3a634926267
Noel Dawe <noel.dawe@gmail.com>
2014-07-03 22:58:18 -0700

    update URL

--- yellowhiggs/info.py
+++ yellowhiggs/info.py
@@ -10,6 +10,6 @@ Author: Noel Dawe
 
 """
 __version__ = '3.0.1'
-URL = 'https://end.web.cern.ch/end/projects/yellowhiggs/'
+URL = 'https://github.com/ndawe/yellowhiggs'
 DOWNLOAD_URL = 'http://pypi.python.org/packages/source/y/yellowhiggs/yellowhiggs-%s.tar.gz' % __version__
 __doc__ %= __version__
